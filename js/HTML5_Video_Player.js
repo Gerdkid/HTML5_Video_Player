b9c6eeb3cbb36c8c80799dcc69f7e0fc0c13e830
Gerdkid <joshgerding@gmail.com>
2016-09-25 01:47:52 -0400

    Setting HTML Video Player

---
+++ js/HTML5_Video_Player.js
@@ -0,0 +1,13 @@
+// const myVideo = document.getElementById('video');
+//
+// function big() {
+//   myVideo.width = 850;
+// }
+//
+// function medium() {
+//   myVideo.width = 700;
+// }
+//
+// function small() {
+//   myVideo.width = 550;
+// }
